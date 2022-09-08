01001a2c4690437156f666ce7b560ef6b93cd0b2
Deyvid William <deyvidwms@gmail.com>
2022-09-08 19:19:48 -0300

    Utilizando express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js';
 
-const rotas = {
-	'/': 'Curso de Node',
-	'/livros': 'Entrei na pag de livros',
-	'/autores': 'Listagem de autores',
-	'/editora': 'Listagem de editoras'
-};
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end(rotas[request.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
 	console.log(`Servidor escutando em http://localhost:${port}`);
 });
