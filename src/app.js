bf705f59c109f7badb088fa9fdeeb3e8b40d4839
Deyvid William <deyvidwms@gmail.com>
2022-09-08 19:57:16 -0300

    terminando o esqueleto de requisições da api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {
         id: 1,
@@ -20,5 +22,33 @@ app.get('/', (request, response) => {
 app.get('/livros', (request, response) => {
     response.status(200).json(livros);
 })
+
+app.get('/livros/:id', (request, response) => {
+    let index = buscaLivro(request.params.id);
+    response.json(livros[index]);
+});
+
+app.post('/livros', (request, response) => {
+    livros.push(request.body);
+    response.status(201).send('Livro cadastrado com sucesso!');
+})
+
+app.put('/livros/:id', (request, response) => {
+    let index = buscaLivro(request.params.id);
+    livros[index].titulo = request.body.titulo;
+    response.json(livros);
+});
+
+app.delete('/livros/:id', (request, response) => {
+    let {id} = request.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    response.send(`Livro ${id} removido com sucesso!`);
+});
+
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id );
+}
 
 export default app
